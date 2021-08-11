14242edacf2fabf98dba6358afbf5a49f9960d1b
Alexander Pico <apico@gladstone.ucsf.edu>
2021-08-11 16:47:27 -0700

    updated setting panel ui

--- src/main/java/org/nrnb/gsoc/enrichment/tasks/EnrichmentAdvancedOptionsTask.java
+++ src/main/java/org/nrnb/gsoc/enrichment/tasks/EnrichmentAdvancedOptionsTask.java
@@ -27,36 +27,36 @@ public class EnrichmentAdvancedOptionsTask extends AbstractTask {
     final CyTable nodeTable;
 
     @Tunable(description = "Adjusted p-value threshold",
-            tooltip = "<html>This is the custom significance threshold.<br/>"+
-                    "Values below the threshold will be excluded.</html>",
-            longDescription = "float between 0 and 1, used to define custom significance threshold.",
+            tooltip = "<html>Values above this threshold will be excluded.</html>",
+            longDescription = "A float value between 0 and 1, used to define a significance threshold for filtering returned results. Default value is 0.05.",
             exampleStringValue = "0.05",
             groups = {"Optional settings"},
             params="slider=false", gravity = 109.0)
     public BoundedDouble user_threshold = new BoundedDouble(0.0, 0.05, 1.0, false, false);
 
-    @Tunable(description = "Decides if electronic annotations should be excluded or not",
-            longDescription = "Unless this is set to true, we only show electronic annotations should be included.",
+    @Tunable(description = "Include inferred GO annotations (IEA)",
+            longDescription = "The default (true) is to include inferred electronic annotations from Gene Ontology.",
             groups = {"Optional settings"},
-            tooltip = "<html>Unless this is set to true, we only show results above the significance threshold.</html>")
+            tooltip = "<html>Uncheck to exclude inferred GO annotations.</html>")
     public boolean no_iea = true;
 
-    @Tunable(description = "Type of testing correction method",
-            tooltip = "Set the type of testing correction method",
-            longDescription = "Set the type of testing correction method",
+    @Tunable(description = "Multiple testing correction",
+            tooltip = "Select the type of multiple testing correction method.",
+            longDescription = "The following multiple testing correction methods are supported: g_SCS (default), bonferroni and fdr.",
             exampleStringValue = "g_SCS",
             groups = {"Optional settings"},
             gravity = 100.0)
     public ListSingleSelection<String> significance_threshold_method;
 
     @Tunable(description="Organism",
-            longDescription="Default species for network queries.",
-            exampleStringValue = "Homo Sapiens",
+            longDescription="The organism associated with the query genes, e.g,. Homo sapiens.",
+            exampleStringValue = "Homo sapiens",
             params="lookup=begins", groups={"Required settings"}, gravity=10.0)
     public ListSingleSelection<String> organism;
 
     @Tunable(description="Gene ID Column",
-            longDescription="Column to choose for getting GeneIDs.",
+    		tooltip = "<html>Select the <b>Node Table</b> column with the query genes.</html>",
+            longDescription="The Node Table column containing the gene symbols or identifiers to be queried.",
             exampleStringValue = "LABEL",
             params="lookup=begins", groups={"Required settings"}, gravity=10.0)
     public ListSingleSelection<String> geneID;
