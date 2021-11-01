4dd1fad5471c408cad4813444e0cc750e2055ba1
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-10-31 18:06:55 -0700

    remove println json body

--- src/main/java/org/nrnb/gsoc/enrichment/RequestEngine/HTTPRequestEngine.java
+++ src/main/java/org/nrnb/gsoc/enrichment/RequestEngine/HTTPRequestEngine.java
@@ -135,7 +135,7 @@ public class HTTPRequestEngine {
         String url = urlConverter.toString();
         HttpPost httpPost = new HttpPost(url);
         String jsonBody = JSONValue.toJSONString(parameters);
-        System.out.println(jsonBody);
+        //System.out.println(jsonBody);
         StringEntity entity = null;
         try {
             entity = new StringEntity(jsonBody);
